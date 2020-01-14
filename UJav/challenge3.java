b8bbd337682ee6d9e2bf95ac342fce5687a0dbd9
dinghy26 <dinghy26@gmail.com>
2020-01-14 01:56:07 +0000

    rev 2 of if else

--- UJav/challenge3.java
+++ UJav/challenge3.java
@@ -6,10 +6,15 @@ public class challenge3 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter the value of x: ");
-        int valueX = scanner.nextInt();
+        int valueX = scanner.nextInt(); // save the value of x in variable valueX
 
         System.out.print("Please enter the value of y: ");
-        int valueY = scanner.nextInt();
+        int valueY = scanner.nextInt(); // save the value of x in variable valueY
+
+        /**
+        if Statement for the conditions of x and x and
+        output the dif. of x and x in each scenario
+         */
 
         if (valueX < valueY) {
             System.out.println("X is smaller than Y by " + (valueY - valueX));
